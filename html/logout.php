2eeef193343311ad081a98bbf6fcf6aba0e1c734
Vedant-Mhatre <vedantmh@gmail.com>
2020-10-13 20:30:01 +0530

    fix logout issue

--- html/logout.php
+++ html/logout.php
@@ -11,7 +11,7 @@ $auth0 = new Auth0([
   ]);
 
 $auth0->logout();
-$return_to = 'https://' . $_SERVER['HTTP_HOST'];
+$return_to = 'http://' . $_SERVER['HTTP_HOST'];
 $logout_url = sprintf('http://%s/v2/logout?client_id=%s&returnTo=%s', 'cave421.us.auth0.com', 'Sxs0ka70IwMzJJW74Fw3LaAFVDxP7Vbw', $return_to);
 header('Location: ' . $logout_url);
 die();
