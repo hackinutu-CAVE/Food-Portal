2525a91e981bad70bfbc51f138ac06cb85d545f1
Vedant-Mhatre <vedantmh@gmail.com>
2020-10-13 19:37:12 +0530

    fix logout error

--- html/logout.php
+++ html/logout.php
@@ -1,8 +1,18 @@
-// logout.php
+<?php
+require 'vendor/autoload.php';
+use Auth0\SDK\Auth0;
 
-// ...
+$auth0 = new Auth0([
+    'domain' => 'cave421.us.auth0.com',
+    'client_id' => 'Sxs0ka70IwMzJJW74Fw3LaAFVDxP7Vbw',
+    'client_secret' => '-0K2p4wvZJ7pNhZeM6cyHJXKEqoCvf5GIwccN_VPPzFyo6MlbH5Zq6uo52c22ZCn',
+    'redirect_uri' => 'https://a588cff11602.ngrok.io/',
+    'scope' => 'openid profile email',
+  ]);
+
 $auth0->logout();
-$return_to = 'http://' . $_SERVER['HTTP_HOST'];
-$logout_url = sprintf('http://%s/v2/logout?client_id=%s&returnTo=%s', 'YOUR_DOMAIN', 'YOUR_CLIENT_ID', $return_to);
+$return_to = 'https://' . $_SERVER['HTTP_HOST'];
+$logout_url = sprintf('http://%s/v2/logout?client_id=%s&returnTo=%s', 'cave421.us.auth0.com', 'Sxs0ka70IwMzJJW74Fw3LaAFVDxP7Vbw', $return_to);
 header('Location: ' . $logout_url);
 die();
+?>
