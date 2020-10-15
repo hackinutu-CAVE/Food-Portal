9fe13b9b724b2d1d60d67b3baaaceb23ec002fbe
AadityaMuley <aadityamuley8190@gmail.com>
2020-10-15 11:13:45 +0530

    Receive page with map added

--- html/home.php
+++ html/home.php
@@ -14,19 +14,12 @@
     </style>
 </head>
 <body>
-	<h3>Google Maps</h3>
-    <div id="map"></div>
-    <script>
-    	function initMap() {
-    		var thane = {lat:19.2183, lng:72.9781};
-    		var map = new google.maps.Map(document.getElementById('map'), {zoom:4, center:thane});
-    		var marker = new google.maps.Marker({position:thane, map:map});
-    	}
-    </script>
-    <script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAsVoY4zW3LEvh1FYV00kTm2LH71ArmzOs&callback=initMap"></script>
-
-
     <h3>Donate here :</h3>
     <input type="button" onclick="window.location='donate.php'" class="Redirect" value="Donate"/>
+
+    <br><br>
+
+    <h3>Reveive here :</h3>
+    <input type="button" onclick="window.location='receive.php'" class="Redirect" value="Donate"/>
 </body>
 </html>
