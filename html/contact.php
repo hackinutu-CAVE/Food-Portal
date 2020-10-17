37f7400913b92bf46189a8d6c63280be6e6270a9
Vedant-Mhatre <vedantmh@gmail.com>
2020-10-17 11:22:22 +0530

    create separate auth file

--- html/contact.php
+++ html/contact.php
@@ -1,21 +1,6 @@
 <?php
-error_reporting(0);
-@ob_start();
-session_start();
-require '/var/www/html/vendor/autoload.php';
-use Auth0\SDK\Auth0;
-
-$auth0 = new Auth0([
-    'domain' => 'cave421.us.auth0.com',
-    'client_id' => 'Sxs0ka70IwMzJJW74Fw3LaAFVDxP7Vbw',
-    'client_secret' => '-0K2p4wvZJ7pNhZeM6cyHJXKEqoCvf5GIwccN_VPPzFyo6MlbH5Zq6uo52c22ZCn',
-    'redirect_uri' => 'https://b34fd096e755.ngrok.io',
-    'scope' => 'openid profile email',
-]);
-
-$userInfo = $auth0->getUser();
+require 'auth.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
--- html/logout.php
+++ html/logout.php
@@ -1,14 +1,6 @@
 <?php
 require 'vendor/autoload.php';
-use Auth0\SDK\Auth0;
-
-$auth0 = new Auth0([
-    'domain' => 'cave421.us.auth0.com',
-    'client_id' => 'Sxs0ka70IwMzJJW74Fw3LaAFVDxP7Vbw',
-    'client_secret' => '-0K2p4wvZJ7pNhZeM6cyHJXKEqoCvf5GIwccN_VPPzFyo6MlbH5Zq6uo52c22ZCn',
-    'redirect_uri' => 'https://b34fd096e755.ngrok.io',
-    'scope' => 'openid profile email',
-  ]);
+require 'auth.php';
 
 $auth0->logout();
 $return_to = 'https://' . $_SERVER['HTTP_HOST'];
