36c2474ad5a77fabed9e4cc7a64fbef7fe9b5864
Vedant-Mhatre <vedantmh@gmail.com>
2020-11-11 17:49:53 +0530

    updated auth0 credentials

--- html/auth.php
+++ html/auth.php
@@ -6,9 +6,9 @@ require '/var/www/html/vendor/autoload.php';
 use Auth0\SDK\Auth0;
 
 $auth0 = new Auth0([
-    'domain' => 'cave421.us.auth0.com',
-    'client_id' => 'Sxs0ka70IwMzJJW74Fw3LaAFVDxP7Vbw',
-    'client_secret' => '-0K2p4wvZJ7pNhZeM6cyHJXKEqoCvf5GIwccN_VPPzFyo6MlbH5Zq6uo52c22ZCn',
+    'domain' => 'food-portal.us.auth0.com',
+    'client_id' => '20lRis7AgcQ7I4qfDwCyGQsZ1ulBwUxd',
+    'client_secret' => 'PuJTEWyhhPEaHZWqnbaZyEwqqwKHHFbGpe6z9XxHH80loP6LUo8xn18yFn-KZtMR',
     'redirect_uri' => 'http://localhost:8080/',
     'scope' => 'openid profile email',
 ]);
